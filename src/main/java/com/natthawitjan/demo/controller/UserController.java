6e2c87dc33ffb6eae927aa700edc75ca775a0a90
Natthawit Janjaroen <natthawit.jan@gmail.com>
2020-08-05 11:42:55 +0700

    add path variable

--- src/main/java/com/natthawitjan/demo/controller/UserController.java
+++ src/main/java/com/natthawitjan/demo/controller/UserController.java
@@ -2,18 +2,25 @@ package com.natthawitjan.demo.controller;
 
 import com.natthawitjan.demo.pojo.User;
 import com.natthawitjan.demo.pojo.UsersResponse;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 @RestController
 public class UserController {
 
-    @GetMapping("users")
-    public UsersResponse[] getAllUsers(){
-        return new UsersResponse[]{
+    @GetMapping({"users", "users/{id}"})
+    public List<UsersResponse> getAllUsers(@PathVariable(required = false) Integer id) {
+        return (id == null) ? Arrays.asList(
                 new UsersResponse(1, "User 1"),
                 new UsersResponse(2, "User 2")
-        };
+        ) : Collections.singletonList(new UsersResponse(id, String.format("User %d", id)));
+
     }
 
 
